f0061aaeb776e3ce8b996a1652f7ce2fd5049e4b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 15:28:30 +0300

    feat(sight): add position component

---
+++ Runtime/Sight/Components/ComponentSightPosition.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentSightPosition : IComponentData
+    {
+        public float3 Value;
+    }
+}
