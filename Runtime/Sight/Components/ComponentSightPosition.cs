80b97596f069816ae75dceb3a8cd46d3c9e5963f
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 15:28:55 +0300

    docs(sight): add position documentation

--- Runtime/Sight/Components/ComponentSightPosition.cs
+++ Runtime/Sight/Components/ComponentSightPosition.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains summarized entity's position for calculations.
+    /// </summary>
     public struct ComponentSightPosition : IComponentData
     {
         public float3 Value;
