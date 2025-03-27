477b077e0bc08e432940fd84076d9b50298955ac
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 15:12:09 +0300

    docs(sight): add source documentation

--- Runtime/Sight/Tags/TagSightSource.cs
+++ Runtime/Sight/Tags/TagSightSource.cs
@@ -2,6 +2,10 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Marks entity to be a source.
+    /// The source can be seen by receivers.
+    /// </summary>
     public struct TagSightSource : IComponentData
     {
     }
