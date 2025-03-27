ea502e1d319b1779cb2c856f6bf588ba9f48c6dc
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 18:16:25 +0300

    feat(sight): add position system

---
+++ Runtime/Sight/Systems/SystemSightPosition.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Perception
+{
+    public partial struct SystemSightPosition : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var commands = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var (transformRO, offsetRO, entity) in SystemAPI
+                         .Query<RefRO<LocalToWorld>, RefRO<ComponentSightOffset>>()
+                         .WithNone<ComponentSightPosition>()
+                         .WithEntityAccess())
+            {
+                commands.AddComponent(entity, new ComponentSightPosition
+                {
+                    Value = transformRO.ValueRO.Value.TransformPoint(in offsetRO.ValueRO.Value),
+                });
+            }
+
+            foreach (var (positionRW, transformRO) in SystemAPI
+                         .Query<RefRW<ComponentSightPosition>, RefRO<LocalToWorld>>()
+                         .WithNone<ComponentSightOffset>())
+            {
+                positionRW.ValueRW.Value = transformRO.ValueRO.Position;
+            }
+
+            foreach (var (positionRW, transformRO, offsetRO) in SystemAPI
+                         .Query<RefRW<ComponentSightPosition>, RefRO<LocalToWorld>, RefRO<ComponentSightOffset>>())
+            {
+                positionRW.ValueRW.Value = transformRO.ValueRO.Value.TransformPoint(in offsetRO.ValueRO.Value);
+            }
+
+            commands.Playback(state.EntityManager);
+        }
+    }
+}
