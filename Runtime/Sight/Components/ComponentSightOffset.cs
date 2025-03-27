37acd1e640005969d49f09e19fc375825130cd93
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 15:17:47 +0300

    docs(sight): add offset documentation

--- Runtime/Sight/Components/ComponentSightOffset.cs
+++ Runtime/Sight/Components/ComponentSightOffset.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds offset to the entity's position during calculations.
+    /// </summary>
     public struct ComponentSightOffset : IComponentData
     {
         public float3 Value;
