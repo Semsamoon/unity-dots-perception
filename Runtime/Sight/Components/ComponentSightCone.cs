fb8b97502fc7038d284d5a75e265d870b06550d4
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 21:21:30 +0300

    docs(sight): add cone documentation

--- Runtime/Sight/Components/ComponentSightCone.cs
+++ Runtime/Sight/Components/ComponentSightCone.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds cone of vision to the receiver.
+    /// </summary>
     public struct ComponentSightCone : IComponentData
     {
         public float2 AnglesTan;
