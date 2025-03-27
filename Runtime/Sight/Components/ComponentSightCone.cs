3d662b5c1b22c39b176b3da581b4c50e90e5e23c
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-27 21:12:24 +0300

    feat(sight): add cone component

---
+++ Runtime/Sight/Components/ComponentSightCone.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentSightCone : IComponentData
+    {
+        public float2 AnglesTan;
+        public float RadiusSquared;
+    }
+}
