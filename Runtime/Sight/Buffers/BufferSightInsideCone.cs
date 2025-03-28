d0d9c1b229a732994310c9496bcc03bf5e644d64
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-28 09:33:58 +0300

    feat(sight): add inside cone buffer

---
+++ Runtime/Sight/Buffers/BufferSightInsideCone.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct BufferSightInsideCone : IBufferElementData
+    {
+        public float3 Position;
+        public Entity Source;
+    }
+}
