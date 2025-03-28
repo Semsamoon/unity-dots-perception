86bea0b44b07ba8d2cdb861b5e23ed8d3b70abf0
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-28 09:35:53 +0300

    docs(sight): add inside cone documentation

--- Runtime/Sight/Buffers/BufferSightInsideCone.cs
+++ Runtime/Sight/Buffers/BufferSightInsideCone.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains sources inside receiver's view cone.
+    /// </summary>
     public struct BufferSightInsideCone : IBufferElementData
     {
         public float3 Position;
