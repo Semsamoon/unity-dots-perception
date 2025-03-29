b6755efd455b6d19a94afc5f6164fa48ec2b8d37
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-29 17:02:32 +0300

    docs(sight): add perceive documentation

--- Runtime/Sight/Buffers/BufferSightPerceive.cs
+++ Runtime/Sight/Buffers/BufferSightPerceive.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains sources perceived by the receiver.
+    /// </summary>
     public struct BufferSightPerceive : IBufferElementData
     {
         public float3 Position;
