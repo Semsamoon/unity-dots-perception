161a9e6d8fe636b04468b116305dbb0a3aaa7e4e
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-29 09:44:15 +0300

    test(sight): rename receivers and sources

--- Tests/Runtime/Sight.cs
+++ Tests/Runtime/Sight.cs
@@ -14,31 +14,31 @@ namespace Perception.Tests
         {
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
-            var entity1 = entityManager.CreateEntity();
-            entityManager.AddComponentData(entity1, new TagSightReceiver());
-            entityManager.AddComponentData(entity1, new LocalToWorld { Value = float4x4.identity });
-            entityManager.AddComponentData(entity1, new ComponentSightOffset { Value = new float3(1, 1, 1) });
+            var receiver = entityManager.CreateEntity();
+            entityManager.AddComponentData(receiver, new TagSightReceiver());
+            entityManager.AddComponentData(receiver, new LocalToWorld { Value = float4x4.identity });
+            entityManager.AddComponentData(receiver, new ComponentSightOffset { Value = new float3(1, 1, 1) });
 
-            var entity2 = entityManager.CreateEntity();
-            entityManager.AddComponentData(entity2, new TagSightSource());
-            entityManager.AddComponentData(entity2, new LocalToWorld { Value = float4x4.Translate(new float3(2, 2, 2)) });
-            entityManager.AddComponentData(entity2, new ComponentSightPosition());
-
-            yield return null;
-
-            Assert.True(entityManager.HasComponent<ComponentSightPosition>(entity1));
-            Assert.False(entityManager.HasComponent<ComponentSightOffset>(entity2));
-
-            Assert.AreEqual(new float3(1, 1, 1), entityManager.GetComponentData<ComponentSightPosition>(entity1).Value);
-            Assert.AreEqual(new float3(2, 2, 2), entityManager.GetComponentData<ComponentSightPosition>(entity2).Value);
-
-            entityManager.SetComponentData(entity1, new ComponentSightOffset { Value = new float3(3, 3, 3) });
-            entityManager.SetComponentData(entity2, new LocalToWorld { Value = float4x4.Translate(new float3(4, 4, 4)) });
+            var source = entityManager.CreateEntity();
+            entityManager.AddComponentData(source, new TagSightSource());
+            entityManager.AddComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(2, 2, 2)) });
+            entityManager.AddComponentData(source, new ComponentSightPosition());
 
             yield return null;
 
-            Assert.AreEqual(new float3(3, 3, 3), entityManager.GetComponentData<ComponentSightPosition>(entity1).Value);
-            Assert.AreEqual(new float3(4, 4, 4), entityManager.GetComponentData<ComponentSightPosition>(entity2).Value);
+            Assert.True(entityManager.HasComponent<ComponentSightPosition>(receiver));
+            Assert.False(entityManager.HasComponent<ComponentSightOffset>(source));
+
+            Assert.AreEqual(new float3(1, 1, 1), entityManager.GetComponentData<ComponentSightPosition>(receiver).Value);
+            Assert.AreEqual(new float3(2, 2, 2), entityManager.GetComponentData<ComponentSightPosition>(source).Value);
+
+            entityManager.SetComponentData(receiver, new ComponentSightOffset { Value = new float3(3, 3, 3) });
+            entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(4, 4, 4)) });
+
+            yield return null;
+
+            Assert.AreEqual(new float3(3, 3, 3), entityManager.GetComponentData<ComponentSightPosition>(receiver).Value);
+            Assert.AreEqual(new float3(4, 4, 4), entityManager.GetComponentData<ComponentSightPosition>(source).Value);
         }
 
         [UnityTest]
