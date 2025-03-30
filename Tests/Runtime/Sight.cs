83a4d16180e2c71be18d64366650db68f3196d2b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-30 10:15:41 +0300

    test(sight): add perceive tests

--- Tests/Runtime/Sight.cs
+++ Tests/Runtime/Sight.cs
@@ -2,7 +2,9 @@
 using NUnit.Framework;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Perception.Tests
@@ -79,6 +81,58 @@ namespace Perception.Tests
             yield return null;
             buffer = entityManager.GetBuffer<BufferSightInsideCone>(receiver);
             Assert.AreEqual(1, buffer.Length);
+        }
+
+        [UnityTest]
+        public IEnumerator Perceive()
+        {
+            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            var awaitPhysics = new WaitForSeconds(0.1f);
+            var collider = Unity.Physics.BoxCollider.Create(new BoxGeometry
+            {
+                Orientation = quaternion.identity, Size = new float3(1, 1, 1)
+            }, CollisionFilter.Default);
+
+            var receiver = entityManager.CreateEntity();
+            entityManager.AddComponentData(receiver, new TagSightReceiver());
+            entityManager.AddComponentData(receiver, new LocalToWorld { Value = float4x4.identity });
+            entityManager.AddComponentData(receiver, new PhysicsCollider { Value = collider });
+            entityManager.AddSharedComponent(receiver, new PhysicsWorldIndex());
+
+            var source = entityManager.CreateEntity();
+            entityManager.AddComponentData(source, new TagSightSource());
+            entityManager.AddComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 5)) });
+            entityManager.AddComponentData(source, new PhysicsCollider { Value = collider });
+            entityManager.AddSharedComponent(source, new PhysicsWorldIndex());
+
+            var obstacle = entityManager.CreateEntity();
+            entityManager.AddComponentData(obstacle, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 3)) });
+            entityManager.AddComponentData(obstacle, new PhysicsCollider { Value = collider });
+            entityManager.AddSharedComponent(obstacle, new PhysicsWorldIndex());
+
+            yield return awaitPhysics;
+            Assert.True(entityManager.HasBuffer<BufferSightPerceive>(receiver));
+            var buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
+            Assert.AreEqual(0, buffer.Length);
+
+            entityManager.SetComponentData(obstacle, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 7)) });
+            yield return awaitPhysics;
+            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
+            Assert.AreEqual(1, buffer.Length);
+
+            Assert.AreEqual(source, buffer[0].Source);
+            Assert.AreEqual(new float3(0, 0, 5), buffer[0].Position);
+
+            entityManager.AddComponentData(receiver, new ComponentSightCone { AnglesTan = new float2(1, 1), RadiusSquared = 25 });
+            yield return awaitPhysics;
+            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
+            Assert.AreEqual(1, buffer.Length);
+
+            entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 5, 5)) });
+            yield return awaitPhysics;
+            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
+            Assert.AreEqual(0, buffer.Length);
         }
     }
 }
