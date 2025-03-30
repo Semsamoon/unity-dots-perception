229b2e432a7463d177c169a2f7690299d701f72e
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-30 10:41:43 +0300

    test(sight): add entity builder and simplify tests

--- Tests/Runtime/Sight.cs
+++ Tests/Runtime/Sight.cs
@@ -15,30 +15,18 @@ namespace Perception.Tests
         public IEnumerator Position()
         {
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-            var receiver = entityManager.CreateEntity();
-            entityManager.AddComponentData(receiver, new TagSightReceiver());
-            entityManager.AddComponentData(receiver, new LocalToWorld { Value = float4x4.identity });
-            entityManager.AddComponentData(receiver, new ComponentSightOffset { Value = new float3(1, 1, 1) });
-
-            var source = entityManager.CreateEntity();
-            entityManager.AddComponentData(source, new TagSightSource());
-            entityManager.AddComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(2, 2, 2)) });
-            entityManager.AddComponentData(source, new ComponentSightPosition());
+            var receiver = new EntityBuilder(entityManager).Receiver().Offset(new float3(1, 1, 1)).Build();
+            var source = new EntityBuilder(entityManager, new float3(2, 2, 2)).Source().Position().Build();
 
             yield return null;
-
             Assert.True(entityManager.HasComponent<ComponentSightPosition>(receiver));
             Assert.False(entityManager.HasComponent<ComponentSightOffset>(source));
-
             Assert.AreEqual(new float3(1, 1, 1), entityManager.GetComponentData<ComponentSightPosition>(receiver).Value);
             Assert.AreEqual(new float3(2, 2, 2), entityManager.GetComponentData<ComponentSightPosition>(source).Value);
 
             entityManager.SetComponentData(receiver, new ComponentSightOffset { Value = new float3(3, 3, 3) });
             entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(4, 4, 4)) });
-
             yield return null;
-
             Assert.AreEqual(new float3(3, 3, 3), entityManager.GetComponentData<ComponentSightPosition>(receiver).Value);
             Assert.AreEqual(new float3(4, 4, 4), entityManager.GetComponentData<ComponentSightPosition>(source).Value);
         }
@@ -47,92 +35,118 @@ namespace Perception.Tests
         public IEnumerator Cone()
         {
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-            var receiver = entityManager.CreateEntity();
-            entityManager.AddComponentData(receiver, new TagSightReceiver());
-            entityManager.AddComponentData(receiver, new LocalToWorld { Value = float4x4.identity });
-            entityManager.AddComponentData(receiver, new ComponentSightCone { AnglesTan = float2.zero, RadiusSquared = 4 });
-
-            var source = entityManager.CreateEntity();
-            entityManager.AddComponentData(source, new TagSightSource());
-            entityManager.AddComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 2)) });
+            var receiver = new EntityBuilder(entityManager).Receiver().Cone(radiusSquared: 4).Build();
+            var source = new EntityBuilder(entityManager, new float3(0, 0, 2)).Source().Build();
 
             yield return null;
             Assert.True(entityManager.HasBuffer<BufferSightInsideCone>(receiver));
-            var buffer = entityManager.GetBuffer<BufferSightInsideCone>(receiver);
-
-            Assert.AreEqual(1, buffer.Length);
-            Assert.AreEqual(source, buffer[0].Source);
-            Assert.AreEqual(new float3(0, 0, 2), buffer[0].Position);
+            Assert.AreEqual(1, entityManager.GetBuffer<BufferSightInsideCone>(receiver).Length);
+            Assert.AreEqual(source, entityManager.GetBuffer<BufferSightInsideCone>(receiver)[0].Source);
+            Assert.AreEqual(new float3(0, 0, 2), entityManager.GetBuffer<BufferSightInsideCone>(receiver)[0].Position);
 
             entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(1, 1, 1)) });
             yield return null;
-            buffer = entityManager.GetBuffer<BufferSightInsideCone>(receiver);
-            Assert.AreEqual(0, buffer.Length);
+            Assert.AreEqual(0, entityManager.GetBuffer<BufferSightInsideCone>(receiver).Length);
 
             entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 3)) });
             yield return null;
-            buffer = entityManager.GetBuffer<BufferSightInsideCone>(receiver);
-            Assert.AreEqual(0, buffer.Length);
+            Assert.AreEqual(0, entityManager.GetBuffer<BufferSightInsideCone>(receiver).Length);
 
             entityManager.SetComponentData(receiver, new ComponentSightCone { AnglesTan = new float2(1, 1), RadiusSquared = 16 });
-
             entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(2, 2, 2)) });
             yield return null;
-            buffer = entityManager.GetBuffer<BufferSightInsideCone>(receiver);
-            Assert.AreEqual(1, buffer.Length);
+            Assert.AreEqual(1, entityManager.GetBuffer<BufferSightInsideCone>(receiver).Length);
         }
 
         [UnityTest]
         public IEnumerator Perceive()
         {
-            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
             var awaitPhysics = new WaitForSeconds(0.1f);
             var collider = Unity.Physics.BoxCollider.Create(new BoxGeometry
             {
                 Orientation = quaternion.identity, Size = new float3(1, 1, 1)
             }, CollisionFilter.Default);
 
-            var receiver = entityManager.CreateEntity();
-            entityManager.AddComponentData(receiver, new TagSightReceiver());
-            entityManager.AddComponentData(receiver, new LocalToWorld { Value = float4x4.identity });
-            entityManager.AddComponentData(receiver, new PhysicsCollider { Value = collider });
-            entityManager.AddSharedComponent(receiver, new PhysicsWorldIndex());
-
-            var source = entityManager.CreateEntity();
-            entityManager.AddComponentData(source, new TagSightSource());
-            entityManager.AddComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 5)) });
-            entityManager.AddComponentData(source, new PhysicsCollider { Value = collider });
-            entityManager.AddSharedComponent(source, new PhysicsWorldIndex());
-
-            var obstacle = entityManager.CreateEntity();
-            entityManager.AddComponentData(obstacle, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 3)) });
-            entityManager.AddComponentData(obstacle, new PhysicsCollider { Value = collider });
-            entityManager.AddSharedComponent(obstacle, new PhysicsWorldIndex());
+            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var receiver = new EntityBuilder(entityManager).Receiver().Collider(collider).Build();
+            var source = new EntityBuilder(entityManager, new float3(0, 0, 5)).Source().Collider(collider).Build();
+            var obstacle = new EntityBuilder(entityManager, new float3(0, 0, 3)).Collider(collider).Build();
 
             yield return awaitPhysics;
             Assert.True(entityManager.HasBuffer<BufferSightPerceive>(receiver));
-            var buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
-            Assert.AreEqual(0, buffer.Length);
+            Assert.AreEqual(0, entityManager.GetBuffer<BufferSightPerceive>(receiver).Length);
 
             entityManager.SetComponentData(obstacle, new LocalToWorld { Value = float4x4.Translate(new float3(0, 0, 7)) });
             yield return awaitPhysics;
-            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
-            Assert.AreEqual(1, buffer.Length);
-
-            Assert.AreEqual(source, buffer[0].Source);
-            Assert.AreEqual(new float3(0, 0, 5), buffer[0].Position);
+            Assert.AreEqual(1, entityManager.GetBuffer<BufferSightPerceive>(receiver).Length);
+            Assert.AreEqual(source, entityManager.GetBuffer<BufferSightPerceive>(receiver)[0].Source);
+            Assert.AreEqual(new float3(0, 0, 5), entityManager.GetBuffer<BufferSightPerceive>(receiver)[0].Position);
 
             entityManager.AddComponentData(receiver, new ComponentSightCone { AnglesTan = new float2(1, 1), RadiusSquared = 25 });
             yield return awaitPhysics;
-            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
-            Assert.AreEqual(1, buffer.Length);
+            Assert.AreEqual(1, entityManager.GetBuffer<BufferSightPerceive>(receiver).Length);
 
             entityManager.SetComponentData(source, new LocalToWorld { Value = float4x4.Translate(new float3(0, 5, 5)) });
             yield return awaitPhysics;
-            buffer = entityManager.GetBuffer<BufferSightPerceive>(receiver);
-            Assert.AreEqual(0, buffer.Length);
+            Assert.AreEqual(0, entityManager.GetBuffer<BufferSightPerceive>(receiver).Length);
+        }
+
+        private struct EntityBuilder
+        {
+            private EntityManager _entityManager;
+            private readonly Entity _entity;
+
+            public EntityBuilder(EntityManager entityManager, float3 position = default)
+            {
+                _entityManager = entityManager;
+                _entity = entityManager.CreateEntity();
+                _entityManager.AddComponentData(_entity, new LocalToWorld { Value = float4x4.Translate(position) });
+            }
+
+            public EntityBuilder Receiver()
+            {
+                _entityManager.AddComponentData(_entity, new TagSightReceiver());
+                return this;
+            }
+
+            public EntityBuilder Source()
+            {
+                _entityManager.AddComponentData(_entity, new TagSightSource());
+                return this;
+            }
+
+            public EntityBuilder Position(float3 position = default)
+            {
+                _entityManager.AddComponentData(_entity, new ComponentSightPosition { Value = position });
+                return this;
+            }
+
+            public EntityBuilder Offset(float3 offset = default)
+            {
+                _entityManager.AddComponentData(_entity, new ComponentSightOffset { Value = offset });
+                return this;
+            }
+
+            public EntityBuilder Cone(float2 anglesTan = default, float radiusSquared = 0)
+            {
+                _entityManager.AddComponentData(_entity, new ComponentSightCone
+                {
+                    AnglesTan = anglesTan, RadiusSquared = radiusSquared
+                });
+                return this;
+            }
+
+            public EntityBuilder Collider(BlobAssetReference<Unity.Physics.Collider> collider = default)
+            {
+                _entityManager.AddComponentData(_entity, new PhysicsCollider { Value = collider });
+                _entityManager.AddSharedComponent(_entity, new PhysicsWorldIndex());
+                return this;
+            }
+
+            public Entity Build()
+            {
+                return _entity;
+            }
         }
     }
 }
