45656337f451d5530546556d62f4fd9c300c976b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-31 10:58:06 +0300

    feat(sight): add cone extend component

---
+++ Runtime/Sight/Components/ComponentSightConeExtend.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentSightConeExtend : IComponentData
+    {
+        public float2 AnglesTan;
+        public float RadiusSquared;
+    }
+}
