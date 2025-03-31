57ed647386e6d8e9143d9f41d16cb5cb367d91ca
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-31 10:55:45 +0300

    docs(sight): add cone offset documentation

--- Runtime/Sight/Components/ComponentSightConeOffset.cs
+++ Runtime/Sight/Components/ComponentSightConeOffset.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds offset to the receiver's cone of vision.
+    /// </summary>
     public struct ComponentSightConeOffset : IComponentData
     {
         public float3 Value;
