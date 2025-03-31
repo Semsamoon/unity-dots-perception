dcfa709db3e4410126a1206bb56ea86d474bcfd4
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-31 10:54:59 +0300

    feat(sight): add cone offset component

---
+++ Runtime/Sight/Components/ComponentSightConeOffset.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentSightConeOffset : IComponentData
+    {
+        public float3 Value;
+    }
+}
