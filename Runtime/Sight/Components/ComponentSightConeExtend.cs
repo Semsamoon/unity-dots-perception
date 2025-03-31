3277189c3437b890ad19e6463af7a432eb8933b1
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-31 11:06:38 +0300

    docs(sight): add cone extend documentation

--- Runtime/Sight/Components/ComponentSightConeExtend.cs
+++ Runtime/Sight/Components/ComponentSightConeExtend.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds extension of the cone of vision for visible sources.
+    /// </summary>
     public struct ComponentSightConeExtend : IComponentData
     {
         public float2 AnglesTan;
