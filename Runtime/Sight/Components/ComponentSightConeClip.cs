78b9ea140d5813dfa0e0830991eb90252090ff43
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-03-31 10:53:55 +0300

    docs(sight): add cone clip documentation

--- Runtime/Sight/Components/ComponentSightConeClip.cs
+++ Runtime/Sight/Components/ComponentSightConeClip.cs
@@ -2,6 +2,9 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds near clipping plane to the receiver's cone of vision.
+    /// </summary>
     public struct ComponentSightConeClip : IComponentData
     {
         public float RadiusSquared;
