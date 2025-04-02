d3a30118b2b81b257e5832bc2d895230db0f86b6
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-02 21:23:26 +0300

    docs(sight): add child documentation

--- Runtime/Sight/Buffers/BufferSightChild.cs
+++ Runtime/Sight/Buffers/BufferSightChild.cs
@@ -2,6 +2,9 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains entities' children for ray casting.
+    /// </summary>
     public struct BufferSightChild : IBufferElementData
     {
         public Entity Value;
