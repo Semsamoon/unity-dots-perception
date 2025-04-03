3cdd666cd02cb6beaa06e08d8d85669819a4e942
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-03 10:07:03 +0300

    docs(sight): add ray offset documentation

--- Runtime/Sight/Buffers/BufferSightRayOffset.cs
+++ Runtime/Sight/Buffers/BufferSightRayOffset.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains rays' offsets for multiple ray casting.
+    /// </summary>
     public struct BufferSightRayOffset : IBufferElementData
     {
         public float3 Value;
