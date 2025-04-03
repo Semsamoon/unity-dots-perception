1099ae514ed5927ca57f1aea3b7632100737759a
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-03 10:04:27 +0300

    feat(sight): add ray offset buffer

---
+++ Runtime/Sight/Buffers/BufferSightRayOffset.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct BufferSightRayOffset : IBufferElementData
+    {
+        public float3 Value;
+    }
+}
