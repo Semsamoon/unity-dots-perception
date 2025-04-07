81ba114f6ca3cca0de2e672f19657df1476a8bda
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-07 16:14:47 +0300

    feat(sight): add memory buffer

---
+++ Runtime/Sight/Buffers/BufferSightMemory.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct BufferSightMemory : IBufferElementData
+    {
+        public float3 Position;
+        public Entity Source;
+        public float Time;
+    }
+}
