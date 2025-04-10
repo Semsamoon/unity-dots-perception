bda46e658edbd0cd43ad1741c8973d0313f7b177
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-10 21:09:53 +0300

    feat(sight): add receiver authoring gizmos

--- Runtime/Sight/Authorings/SightSenseReceiverAuthoring.cs
+++ Runtime/Sight/Authorings/SightSenseReceiverAuthoring.cs
@@ -106,6 +106,61 @@ namespace Perception
 
             _memoryTime = math.max(0, _memoryTime);
         }
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            if (Application.isPlaying)
+            {
+                return;
+            }
+
+            var transform = this.transform;
+            var coneHalfAngles = math.radians(_coneAnglesDegrees) / 2;
+            var extendHalfAngles = math.radians(_coneAnglesDegrees + _extendAnglesDegrees) / 2;
+            var position = transform.TransformPoint(_offset);
+
+            DrawCone(position, transform.rotation, 0, _coneRadius + _extendRadius, extendHalfAngles, Color.yellow);
+            DrawCone(position, transform.rotation, _clipRadius, _coneRadius, coneHalfAngles, Color.green);
+            DrawCone(position, transform.rotation, 0, _clipRadius, extendHalfAngles, Color.gray);
+        }
+
+        public static void DrawCone(float3 position, quaternion rotation, float clip, float radius, float2 halfAnglesRadians, Color color, int sparsity = 1000)
+        {
+            var halfAngleX = halfAnglesRadians.x;
+            var halfAngleY = halfAnglesRadians.y;
+
+            var eulerAngles = new float3x4(
+                new float3(halfAngleY, halfAngleX, 0),
+                new float3(-halfAngleY, halfAngleX, 0),
+                new float3(halfAngleY, -halfAngleX, 0),
+                new float3(-halfAngleY, -halfAngleX, 0));
+
+            for (var i = 0; i < 4; i++)
+            {
+                var quaternion = Unity.Mathematics.quaternion.Euler(eulerAngles[i]);
+                var start = math.rotate(rotation, math.rotate(quaternion, new float3(0, 0, clip)));
+                var end = math.rotate(rotation, math.rotate(quaternion, new float3(0, 0, radius)));
+
+                Debug.DrawLine(position + start, position + end, color);
+            }
+
+            var offsetY = math.sin(halfAngleY) * radius + radius;
+            var radiusY = math.sqrt(2 * radius * math.abs(offsetY) - offsetY * offsetY);
+            var verticalsOffset = math.rotate(rotation, new Vector3(0, offsetY - radius, 0));
+
+            DebugAdvanced.DrawCurve(position + verticalsOffset, rotation, radiusY, halfAngleX * 2, color, sparsity);
+            DebugAdvanced.DrawCurve(position - verticalsOffset, rotation, radiusY, halfAngleX * 2, color, sparsity);
+
+            var segments = (int)(halfAngleX * radius / 1000) + (halfAngleX > 1 ? 2 : 1);
+
+            for (var i = 0; i <= segments; i++)
+            {
+                var angle = math.lerp(-halfAngleX, halfAngleX, (float)i / segments);
+                var quaternion = math.mul(rotation, Unity.Mathematics.quaternion.Euler(0, angle, math.PIHALF));
+
+                DebugAdvanced.DrawCurve(position, quaternion, radius, halfAngleY * 2, color, sparsity);
+            }
+        }
 #endif
     }
 }
