be971bb73dd407d4363049a3d56776168cbc18bb
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-12 09:42:48 +0300

    feat(sight): add source authoring

---
+++ Runtime/Sight/Authorings/SightSenseSourceAuthoring.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Perception
+{
+    public class SightSenseSourceAuthoring : MonoBehaviour
+    {
+        [SerializeField] protected float3 _offset;
+        [SerializeField] protected GameObject[] _children;
+
+        public class Baker : Baker<SightSenseSourceAuthoring>
+        {
+            public override void Bake(SightSenseSourceAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new TagSightSource());
+
+                if (math.any(authoring._offset != float3.zero))
+                {
+                    AddComponent(entity, new ComponentSightOffset { Source = authoring._offset });
+                }
+
+                if (authoring._children is { Length: > 0 })
+                {
+                    AddBuffer<BufferSightChild>(entity);
+
+                    foreach (var child in authoring._children)
+                    {
+                        if (child)
+                        {
+                            AppendToBuffer(entity, new BufferSightChild { Value = GetEntity(child, TransformUsageFlags.Dynamic) });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
