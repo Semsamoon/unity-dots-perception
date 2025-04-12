1786687b4b611fb909201eb61ffed2bd522ebc95
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-12 15:07:51 +0300

    docs(sight): add debug documentation

--- Editor/Sight/TagSightDebug.cs
+++ Editor/Sight/TagSightDebug.cs
@@ -2,6 +2,9 @@
 
 namespace Perception.Editor
 {
+    /// <summary>
+    /// Marks receiver to debug its cone of vision.
+    /// </summary>
     public struct TagSightDebug : IComponentData, IEnableableComponent
     {
     }
