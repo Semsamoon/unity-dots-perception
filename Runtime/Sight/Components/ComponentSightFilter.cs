282b58fbec8da5ae36f737ad69bc8c5d9b0ad966
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-17 20:27:02 +0300

    docs(sight): add filter documentation

--- Runtime/Sight/Components/ComponentSightFilter.cs
+++ Runtime/Sight/Components/ComponentSightFilter.cs
@@ -3,6 +3,9 @@ using Unity.Physics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds collision filter to receiver for ray casting.
+    /// </summary>
     public struct ComponentSightFilter : IComponentData
     {
         public CollisionFilter Value;
