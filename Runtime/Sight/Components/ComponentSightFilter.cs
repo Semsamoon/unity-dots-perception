41e697ba7c190992bb1f6b06fb826b966e2128b7
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-17 20:23:26 +0300

    feat(sight): add filter component

---
+++ Runtime/Sight/Components/ComponentSightFilter.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+
+namespace Perception
+{
+    public struct ComponentSightFilter : IComponentData
+    {
+        public CollisionFilter Value;
+    }
+}
