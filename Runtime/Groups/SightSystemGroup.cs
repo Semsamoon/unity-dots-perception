d0566f41253e9a7f1ed238eb680bce4e44692cec
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-22 23:17:45 +0300

    feat(sight): add sight system group

---
+++ Runtime/Groups/SightSystemGroup.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Perception
+{
+    [UpdateInGroup(typeof(PerceptionSystemGroup))]
+    public partial class SightSystemGroup : ComponentSystemGroup
+    {
+    }
+}
