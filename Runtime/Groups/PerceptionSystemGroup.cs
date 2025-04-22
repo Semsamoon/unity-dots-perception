ecbb6f07ffda5ba048f995adae64da9482d25e14
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-22 23:14:47 +0300

    feat: add perception system group

---
+++ Runtime/Groups/PerceptionSystemGroup.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Perception
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public partial class PerceptionSystemGroup : ComponentSystemGroup
+    {
+    }
+}
