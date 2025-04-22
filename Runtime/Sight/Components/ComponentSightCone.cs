25ce5832b7af2491007ad1c9190e4f97288d31e1
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-23 00:49:34 +0300

    feat(sight): add components extensions

--- Runtime/Sight/Components/ComponentSightCone.cs
+++ Runtime/Sight/Components/ComponentSightCone.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace Perception
 {
@@ -10,5 +12,27 @@ namespace Perception
     {
         public float2 AnglesCos;
         public float RadiusSquared;
+    }
+
+    [BurstCompile]
+    public static class ComponentSightConeExtensions
+    {
+        [BurstCompile]
+        public static bool IsInside(this in ComponentSightCone cone, in float3 origin, in float3 target, in LocalToWorld transform, float clipSquared = 0)
+        {
+            var difference = target - origin;
+            var distanceSquared = math.lengthsq(difference);
+
+            if (distanceSquared > cone.RadiusSquared || distanceSquared < clipSquared)
+            {
+                return false;
+            }
+
+            var directionLocal = transform.Value.InverseTransformDirection(difference);
+            var squared = directionLocal * directionLocal;
+
+            return directionLocal.z / math.sqrt(squared.x + squared.z) >= cone.AnglesCos.x
+                   && math.sqrt((squared.x + squared.z) / (squared.x + squared.y + squared.z)) >= cone.AnglesCos.y;
+        }
     }
 }
--- Runtime/Sight/Components/ComponentSightExtend.cs
+++ Runtime/Sight/Components/ComponentSightExtend.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace Perception
 {
@@ -10,5 +12,27 @@ namespace Perception
     {
         public float2 AnglesCos;
         public float RadiusSquared;
+    }
+
+    [BurstCompile]
+    public static class ComponentSightExtendExtensions
+    {
+        [BurstCompile]
+        public static bool IsInside(this in ComponentSightExtend extend, in float3 origin, in float3 target, in LocalToWorld transform, float clipSquared = 0)
+        {
+            var difference = target - origin;
+            var distanceSquared = math.lengthsq(difference);
+
+            if (distanceSquared > extend.RadiusSquared || distanceSquared < clipSquared)
+            {
+                return false;
+            }
+
+            var directionLocal = transform.Value.InverseTransformDirection(difference);
+            var squared = directionLocal * directionLocal;
+
+            return directionLocal.z / math.sqrt(squared.x + squared.z) >= extend.AnglesCos.x
+                   && math.sqrt((squared.x + squared.z) / (squared.x + squared.y + squared.z)) >= extend.AnglesCos.y;
+        }
     }
 }
