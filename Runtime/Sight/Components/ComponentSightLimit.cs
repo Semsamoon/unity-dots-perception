8d957777001f155e16c423b187f3d918a10908c7
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-23 14:01:59 +0300

    feat(sight): add limit component

---
+++ Runtime/Sight/Components/ComponentSightLimit.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace Perception
+{
+    public struct ComponentSightLimit : IComponentData
+    {
+        public int ChunksAmountPosition;
+        public int ChunksAmountMemory;
+        public int ChunksAmountCone;
+        public int ChunksAmountPerceiveSingle;
+        public int ChunksAmountPerceiveOffset;
+    }
+}
