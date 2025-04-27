0cf16ccad0451088eeb7bddcd0bcfe1fac98c6b6
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-27 20:48:48 +0300

    feat(hearing): add position component

---
+++ Runtime/Hearing/Components/ComponentHearingPosition.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentHearingPosition : IComponentData
+    {
+        public float3 Current;
+        public float3 Previous;
+    }
+}
