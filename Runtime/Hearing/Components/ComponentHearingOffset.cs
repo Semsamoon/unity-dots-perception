9dc43e4d743e949b0ae832afc447a6edabb827d2
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-27 20:44:54 +0300

    feat(hearing): add offset component

---
+++ Runtime/Hearing/Components/ComponentHearingOffset.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct ComponentHearingOffset : IComponentData
+    {
+        public float3 Value;
+    }
+}
