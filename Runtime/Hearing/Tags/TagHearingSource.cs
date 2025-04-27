25b80cc834485acbd5f42472ac656b0a7a7194db
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-27 20:43:44 +0300

    docs(hearing): add source documentation

--- Runtime/Hearing/Tags/TagHearingSource.cs
+++ Runtime/Hearing/Tags/TagHearingSource.cs
@@ -2,6 +2,10 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Marks entity to be a source.
+    /// The source can be heard by receivers.
+    /// </summary>
     public struct TagHearingSource : IComponentData
     {
     }
