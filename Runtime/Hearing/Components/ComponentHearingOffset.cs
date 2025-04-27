1349e6db6a548ad21c784bfdcf55a97d1068b76b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-27 20:46:59 +0300

    docs(hearing): add offset documentation

--- Runtime/Hearing/Components/ComponentHearingOffset.cs
+++ Runtime/Hearing/Components/ComponentHearingOffset.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds offset to the receiver's and source's positions.
+    /// </summary>
     public struct ComponentHearingOffset : IComponentData
     {
         public float3 Value;
