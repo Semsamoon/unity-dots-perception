95a87fca7ca3341bb2353703dfd68cff870fa93b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-27 20:49:26 +0300

    docs(hearing): add position documentation

--- Runtime/Hearing/Components/ComponentHearingPosition.cs
+++ Runtime/Hearing/Components/ComponentHearingPosition.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds summarized receiver's and source's positions for calculations.
+    /// </summary>
     public struct ComponentHearingPosition : IComponentData
     {
         public float3 Current;
