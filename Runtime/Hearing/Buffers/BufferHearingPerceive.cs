a852b014cf4b72866c34f1e69359c41fea927432
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-28 23:31:47 +0300

    docs(hearing): add perceive documentation

--- Runtime/Hearing/Buffers/BufferHearingPerceive.cs
+++ Runtime/Hearing/Buffers/BufferHearingPerceive.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace Perception
 {
+    /// <summary>
+    /// Contains sources perceived by the receiver.
+    /// </summary>
     public struct BufferHearingPerceive : IBufferElementData
     {
         public float3 Position;
