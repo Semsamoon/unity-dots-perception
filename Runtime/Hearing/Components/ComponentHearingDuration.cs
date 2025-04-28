5a2ee0bbb3372b5938b67a8ee66a026d3e6660f7
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-28 22:54:22 +0300

    docs(hearing): add duration documentation

--- Runtime/Hearing/Components/ComponentHearingDuration.cs
+++ Runtime/Hearing/Components/ComponentHearingDuration.cs
@@ -2,6 +2,9 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds duration of wave propagation to the source.
+    /// </summary>
     public struct ComponentHearingDuration : IComponentData
     {
         public float Time;
