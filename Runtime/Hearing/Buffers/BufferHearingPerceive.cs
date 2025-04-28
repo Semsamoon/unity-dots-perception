4e11b8171d7aeb7ef9c1d94704a0786ecf8521ca
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-28 23:30:58 +0300

    feat(hearing): add perceive buffer

---
+++ Runtime/Hearing/Buffers/BufferHearingPerceive.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct BufferHearingPerceive : IBufferElementData
+    {
+        public float3 Position;
+        public Entity Source;
+    }
+}
