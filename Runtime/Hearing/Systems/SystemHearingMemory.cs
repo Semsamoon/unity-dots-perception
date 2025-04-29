de4cd554bfb048a6387599c3021b8e8fead98dad
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-29 22:48:41 +0300

    feat(hearing): add memory system

---
+++ Runtime/Hearing/Systems/SystemHearingMemory.cs
@@ -0,0 +1,51 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace Perception
+{
+    public partial struct SystemHearingMemory : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var commands = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var receiver in SystemAPI.QueryBuilder()
+                         .WithAll<TagHearingReceiver, ComponentHearingMemory>()
+                         .WithNone<BufferHearingMemory>()
+                         .Build().ToEntityArray(Allocator.Temp))
+            {
+                commands.AddBuffer<BufferHearingMemory>(receiver);
+            }
+
+            commands.Playback(state.EntityManager);
+
+            var buffersMemory = SystemAPI.GetBufferLookup<BufferHearingMemory>();
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            foreach (var receiver in SystemAPI.QueryBuilder()
+                         .WithAll<TagHearingReceiver, BufferHearingMemory>()
+                         .Build().ToEntityArray(Allocator.Temp))
+            {
+                var bufferMemory = buffersMemory[receiver];
+
+                for (var j = bufferMemory.Length - 1; j >= 0; j--)
+                {
+                    bufferMemory.ElementAt(j).Time -= deltaTime;
+
+                    if (bufferMemory[j].Time <= 0)
+                    {
+                        bufferMemory.RemoveAtSwapBack(j);
+                    }
+                }
+            }
+        }
+    }
+}
