97fb4aeac92c91af2df8bf28a72e8946730434a8
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-29 23:03:48 +0300

    feat(hearing): use memory in perceive system

--- Runtime/Hearing/Systems/SystemHearingPerceive.cs
+++ Runtime/Hearing/Systems/SystemHearingPerceive.cs
@@ -29,10 +29,63 @@ namespace Perception
             commands.Playback(state.EntityManager);
 
             var buffersPerceive = SystemAPI.GetBufferLookup<BufferHearingPerceive>();
+            var buffersMemory = SystemAPI.GetBufferLookup<BufferHearingMemory>();
+
+            foreach (var (positionRO, memoryRO, receiver) in SystemAPI
+                         .Query<RefRO<ComponentHearingPosition>, RefRO<ComponentHearingMemory>>()
+                         .WithAll<TagHearingReceiver>()
+                         .WithEntityAccess())
+            {
+                var bufferPerceive = buffersPerceive[receiver];
+                var bufferMemory = buffersMemory[receiver];
+                ref readonly var position = ref positionRO.ValueRO;
+
+                var perceiveLength = bufferPerceive.Length;
+
+                foreach (var (sourcePositionRO, radiusRO, source) in SystemAPI
+                             .Query<RefRO<ComponentHearingPosition>, RefRO<ComponentHearingRadius>>()
+                             .WithAll<TagHearingSource>()
+                             .WithEntityAccess())
+                {
+                    var isPerceived = bufferPerceive.Contains(in source, perceiveLength, out var index, out var perceive);
+
+                    if (isPerceived)
+                    {
+                        bufferPerceive.RemoveAtSwapBack(index, --perceiveLength);
+                    }
+
+                    ref readonly var sourcePosition = ref sourcePositionRO.ValueRO;
+                    ref readonly var radius = ref radiusRO.ValueRO;
+
+                    var distanceCurrentSquared = math.distancesq(position.Current, sourcePosition.Current);
+                    var distancePreviousSquared = math.distancesq(position.Previous, sourcePosition.Previous);
+
+                    if ((distanceCurrentSquared <= radius.CurrentSquared && distanceCurrentSquared >= radius.InternalCurrentSquared)
+                        || (distanceCurrentSquared > radius.CurrentSquared && distancePreviousSquared < radius.InternalPreviousSquared)
+                        || (distanceCurrentSquared < radius.InternalCurrentSquared && distancePreviousSquared > radius.PreviousSquared))
+                    {
+                        bufferPerceive.Add(new BufferHearingPerceive
+                        {
+                            Position = sourcePosition.Current,
+                            Source = source,
+                        });
+                        bufferMemory.Remove(source);
+                        continue;
+                    }
+
+                    if (isPerceived)
+                    {
+                        bufferMemory.Add(new BufferHearingMemory { Position = perceive.Position, Source = perceive.Source, Time = memoryRO.ValueRO.Time });
+                    }
+                }
+
+                bufferPerceive.ToMemories(perceiveLength, ref bufferMemory, memoryRO.ValueRO.Time);
+            }
 
             foreach (var (positionRO, receiver) in SystemAPI
                          .Query<RefRO<ComponentHearingPosition>>()
                          .WithAll<TagHearingReceiver>()
+                         .WithNone<ComponentHearingMemory>()
                          .WithEntityAccess())
             {
                 var bufferPerceive = buffersPerceive[receiver];
