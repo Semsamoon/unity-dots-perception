69f4f1cf6023d805d86bde0d29f4a4ec529720f9
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-29 22:46:02 +0300

    feat(hearing): add memory buffer

---
+++ Runtime/Hearing/Buffers/BufferHearingMemory.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Perception
+{
+    public struct BufferHearingMemory : IBufferElementData
+    {
+        public float3 Position;
+        public Entity Source;
+        public float Time;
+    }
+}
