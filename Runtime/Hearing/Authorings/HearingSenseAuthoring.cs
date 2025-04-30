fea60e298bec44fd3f64e858522d64d77e95dd4a
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-04-30 10:33:42 +0300

    feat(hearing): add authoring

---
+++ Runtime/Hearing/Authorings/HearingSenseAuthoring.cs
@@ -0,0 +1,78 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Perception
+{
+    public class HearingSenseAuthoring : MonoBehaviour
+    {
+        [SerializeField] protected bool _isReceiver;
+
+        [SerializeField] protected float _memoryTime;
+
+        [Space]
+        [SerializeField] protected bool _isSource;
+
+        [SerializeField] protected float _speed;
+        [SerializeField] protected float _maxRange;
+
+        [SerializeField] protected float _duration;
+
+        [SerializeField] protected float3 _offset;
+
+        public class Baker : Baker<HearingSenseAuthoring>
+        {
+            public override void Bake(HearingSenseAuthoring authoring)
+            {
+                if (!authoring._isReceiver && !authoring._isSource)
+                {
+                    return;
+                }
+
+                var entity = GetEntity(authoring._isReceiver ? TransformUsageFlags.Dynamic : TransformUsageFlags.None);
+
+                if (authoring._isReceiver)
+                {
+                    AddComponent(entity, new TagHearingReceiver());
+
+                    if (authoring._memoryTime > 0)
+                    {
+                        AddComponent(entity, new ComponentHearingMemory { Time = authoring._memoryTime });
+                    }
+                }
+
+                if (authoring._isSource)
+                {
+                    AddComponent(entity, new TagHearingSource());
+                    AddComponent(entity, new ComponentHearingSphere
+                    {
+                        Speed = authoring._speed,
+                        RangeSquared = authoring._maxRange * authoring._maxRange,
+                    });
+
+                    if (authoring._duration >= 0)
+                    {
+                        AddComponent(entity, new ComponentHearingDuration { Time = authoring._duration });
+                    }
+                }
+
+                if (math.any(authoring._offset != float3.zero))
+                {
+                    AddComponent(entity, new ComponentHearingOffset { Value = authoring._offset });
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+            _memoryTime = math.max(0, _memoryTime);
+
+            _speed = math.max(0, _speed);
+            _maxRange = math.max(0, _maxRange);
+
+            _duration = _duration is >= 0 or > -1 and < -0.5f ? math.max(0, _duration) : -1;
+        }
+#endif
+    }
+}
