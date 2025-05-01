6acdb3766360d769ea500c6c2fd214e8cd28851d
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-01 09:59:27 +0300

    feat(hearing): add debug settings component

---
+++ Editor/Hearing/ComponentHearingDebug.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Perception.Editor
+{
+    public struct ComponentHearingDebug : IComponentData
+    {
+        public Color ColorSourceSphere;
+        public Color ColorSourceInternal;
+
+        public Color ColorSourcePerceived;
+        public Color ColorSourceMemorized;
+
+        public float3 SizeOctahedron;
+        public float ScaleOctahedronBig;
+        public float ScaleOctahedronSmall;
+
+        public static ComponentHearingDebug Default => new()
+        {
+            ColorSourceSphere = Color.green,
+            ColorSourceInternal = Color.red,
+
+            ColorSourcePerceived = Color.green,
+            ColorSourceMemorized = Color.yellow,
+
+            SizeOctahedron = new float3(0.5f, 1, 0.5f),
+            ScaleOctahedronBig = 0.5f,
+            ScaleOctahedronSmall = 0.25f,
+        };
+    }
+}
