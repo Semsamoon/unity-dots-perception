6bf16eed3832438e067bd8aad21aa9e84b0742df
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-01 09:52:07 +0300

    docs(hearing): add debug documentation

--- Editor/Hearing/TagHearingDebug.cs
+++ Editor/Hearing/TagHearingDebug.cs
@@ -2,6 +2,9 @@
 
 namespace Perception.Editor
 {
+    /// <summary>
+    /// Marks receiver or source to debug its hearing sense.
+    /// </summary>
     public struct TagHearingDebug : IComponentData, IEnableableComponent
     {
     }
