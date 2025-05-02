90a86401ed5fa766ee37924d23b045fdead6b2f8
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-02 11:27:00 +0300

    perf(hearing): add jobs to debug

--- Editor/Hearing/SystemHearingDebug.cs
+++ Editor/Hearing/SystemHearingDebug.cs
@@ -1,14 +1,26 @@
-﻿using Unity.Collections;
+﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
 
 namespace Perception.Editor
 {
-    [UpdateInGroup(typeof(HearingSystemGroup), OrderLast = true)]
+    [BurstCompile, UpdateInGroup(typeof(HearingSystemGroup), OrderLast = true)]
     public partial struct SystemHearingDebug : ISystem
     {
+        private EntityQuery _queryWithoutDebug;
+
+        private EntityQuery _querySphereWithDurationWithTransform;
+        private EntityQuery _querySphereWithDuration;
+        private EntityQuery _querySphereWithTransform;
+        private EntityQuery _querySphere;
+        private EntityQuery _queryMemorized;
+        private EntityQuery _queryPerceived;
+
+        private ComponentLookup<ComponentHearingPosition> _lookupPosition;
+
+        [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<ComponentHearingDebug>();
@@ -17,22 +29,34 @@ namespace Perception.Editor
             {
                 state.EntityManager.CreateSingleton(ComponentHearingDebug.Default);
             }
+
+            _queryWithoutDebug = SystemAPI.QueryBuilder().WithAny<TagHearingReceiver, TagHearingSource>().WithNone<TagHearingDebug>().Build();
+
+            _querySphereWithDurationWithTransform = SystemAPI.QueryBuilder().WithAll<TagHearingSource, TagHearingDebug>()
+                .WithAll<ComponentHearingPosition, ComponentHearingRadius>().WithAll<ComponentHearingDuration, LocalToWorld>().Build();
+            _querySphereWithDuration = SystemAPI.QueryBuilder().WithAll<TagHearingSource, TagHearingDebug>()
+                .WithAll<ComponentHearingPosition, ComponentHearingRadius>().WithAll<ComponentHearingDuration>().WithNone<LocalToWorld>().Build();
+            _querySphereWithTransform = SystemAPI.QueryBuilder().WithAll<TagHearingSource, TagHearingDebug>()
+                .WithAll<ComponentHearingPosition, ComponentHearingRadius>().WithAll<LocalToWorld>().Build();
+            _querySphere = SystemAPI.QueryBuilder().WithAll<TagHearingSource, TagHearingDebug>()
+                .WithAll<ComponentHearingPosition, ComponentHearingRadius>().WithNone<LocalToWorld>().Build();
+            _queryMemorized = SystemAPI.QueryBuilder().WithAll<TagHearingReceiver, TagHearingDebug>().WithAll<ComponentHearingPosition, BufferHearingMemory>().Build();
+            _queryPerceived = SystemAPI.QueryBuilder().WithAll<TagHearingReceiver, TagHearingDebug>().WithAll<ComponentHearingPosition, BufferHearingPerceive>().Build();
+
+            _lookupPosition = SystemAPI.GetComponentLookup<ComponentHearingPosition>(isReadOnly: true);
         }
 
+        [BurstCompile]
         public void OnDestroy(ref SystemState state)
         {
         }
 
+        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var commands = new EntityCommandBuffer(Allocator.Temp);
 
-            foreach (var source in SystemAPI
-                         .QueryBuilder()
-                         .WithAny<TagHearingReceiver, TagHearingSource>()
-                         .WithNone<TagHearingDebug>()
-                         .Build()
-                         .ToEntityArray(Allocator.Temp))
+            foreach (var source in _queryWithoutDebug.ToEntityArray(Allocator.Temp))
             {
                 commands.AddComponent<TagHearingDebug>(source);
                 commands.SetComponentEnabled<TagHearingDebug>(source, false);
@@ -40,88 +64,100 @@ namespace Perception.Editor
 
             commands.Playback(state.EntityManager);
 
-            var buffersPerceive = SystemAPI.GetBufferLookup<BufferHearingPerceive>();
-            var buffersMemory = SystemAPI.GetBufferLookup<BufferHearingMemory>();
-            var lookupPosition = SystemAPI.GetComponentLookup<ComponentHearingPosition>();
+            _lookupPosition.Update(ref state);
 
             ref readonly var debug = ref SystemAPI.GetSingletonRW<ComponentHearingDebug>().ValueRO;
 
-            foreach (var (positionRO, receiver) in SystemAPI
-                         .Query<RefRO<ComponentHearingPosition>>()
-                         .WithAll<TagHearingReceiver, TagHearingDebug>()
-                         .WithAll<ComponentHearingMemory>()
-                         .WithEntityAccess())
-            {
-                var position = positionRO.ValueRO.Current;
-                var bufferMemory = buffersMemory[receiver];
+            var jobHandle = new JobDebugSphereWithDurationWithTransform { Debug = debug }.ScheduleParallel(_querySphereWithDurationWithTransform, state.Dependency);
+            jobHandle = new JobDebugSphereWithDuration { Debug = debug }.ScheduleParallel(_querySphereWithDuration, jobHandle);
+            jobHandle = new JobDebugSphereWithTransform { Debug = debug }.ScheduleParallel(_querySphereWithTransform, jobHandle);
+            jobHandle = new JobDebugSphere { Debug = debug }.ScheduleParallel(_querySphere, jobHandle);
+            jobHandle = new JobDebugMemorized { Debug = debug, LookupPosition = _lookupPosition }.ScheduleParallel(_queryMemorized, jobHandle);
+            state.Dependency = new JobDebugPerceived { Debug = debug }.ScheduleParallel(_queryPerceived, jobHandle);
+        }
 
+        [BurstCompile]
+        private partial struct JobDebugSphereWithDurationWithTransform : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [BurstCompile]
+            public void Execute(in ComponentHearingPosition position, in ComponentHearingRadius radius, in LocalToWorld transform)
+            {
+                DebugAdvanced.DrawSphere(position.Current, transform.Rotation, math.sqrt(radius.InternalCurrentSquared), Debug.ColorSourceInternal);
+            }
+        }
+
+        [BurstCompile]
+        private partial struct JobDebugSphereWithDuration : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [BurstCompile]
+            public void Execute(in ComponentHearingPosition position, in ComponentHearingRadius radius)
+            {
+                DebugAdvanced.DrawSphere(position.Current, quaternion.identity, math.sqrt(radius.InternalCurrentSquared), Debug.ColorSourceInternal);
+            }
+        }
+
+        [BurstCompile]
+        private partial struct JobDebugSphereWithTransform : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [BurstCompile]
+            public void Execute(in ComponentHearingPosition position, in ComponentHearingRadius radius, in LocalToWorld transform)
+            {
+                DebugAdvanced.DrawSphere(position.Current, transform.Rotation, math.sqrt(radius.CurrentSquared), Debug.ColorSourceSphere);
+            }
+        }
+
+        [BurstCompile]
+        private partial struct JobDebugSphere : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [BurstCompile]
+            public void Execute(in ComponentHearingPosition position, in ComponentHearingRadius radius)
+            {
+                DebugAdvanced.DrawSphere(position.Current, quaternion.identity, math.sqrt(radius.CurrentSquared), Debug.ColorSourceSphere);
+            }
+        }
+
+        [BurstCompile]
+        private partial struct JobDebugMemorized : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [ReadOnly] public ComponentLookup<ComponentHearingPosition> LookupPosition;
+
+            [BurstCompile]
+            public void Execute(in DynamicBuffer<BufferHearingMemory> bufferMemory, in ComponentHearingPosition position)
+            {
                 foreach (var memory in bufferMemory)
                 {
-                    var sourcePosition = lookupPosition[memory.Source].Current;
-                    Debug.DrawLine(position, memory.Position, debug.ColorSourceMemorized);
-                    Debug.DrawLine(memory.Position, sourcePosition, debug.ColorSourceMemorized);
-                    DebugAdvanced.DrawOctahedron(memory.Position, debug.SizeOctahedron * debug.ScaleOctahedronSmall, debug.ColorSourceMemorized);
-                    DebugAdvanced.DrawOctahedron(sourcePosition, debug.SizeOctahedron * debug.ScaleOctahedronBig, debug.ColorSourceMemorized);
+                    var sourcePosition = LookupPosition[memory.Source].Current;
+                    UnityEngine.Debug.DrawLine(position.Current, memory.Position, Debug.ColorSourceMemorized);
+                    UnityEngine.Debug.DrawLine(memory.Position, sourcePosition, Debug.ColorSourceMemorized);
+                    DebugAdvanced.DrawOctahedron(memory.Position, Debug.SizeOctahedron * Debug.ScaleOctahedronSmall, Debug.ColorSourceMemorized);
+                    DebugAdvanced.DrawOctahedron(sourcePosition, Debug.SizeOctahedron * Debug.ScaleOctahedronBig, Debug.ColorSourceMemorized);
                 }
             }
+        }
 
-            foreach (var (positionRO, receiver) in SystemAPI
-                         .Query<RefRO<ComponentHearingPosition>>()
-                         .WithAll<TagHearingReceiver, TagHearingDebug>()
-                         .WithEntityAccess())
+        [BurstCompile]
+        private partial struct JobDebugPerceived : IJobEntity
+        {
+            [ReadOnly] public ComponentHearingDebug Debug;
+
+            [BurstCompile]
+            public void Execute(in DynamicBuffer<BufferHearingPerceive> bufferPerceive, in ComponentHearingPosition position)
             {
-                var position = positionRO.ValueRO.Current;
-                var bufferPerceive = buffersPerceive[receiver];
-
                 foreach (var perceive in bufferPerceive)
                 {
-                    Debug.DrawLine(position, perceive.Position, debug.ColorSourcePerceived);
-                    DebugAdvanced.DrawOctahedron(perceive.Position, debug.SizeOctahedron * debug.ScaleOctahedronBig, debug.ColorSourcePerceived);
+                    UnityEngine.Debug.DrawLine(position.Current, perceive.Position, Debug.ColorSourcePerceived);
+                    DebugAdvanced.DrawOctahedron(perceive.Position, Debug.SizeOctahedron * Debug.ScaleOctahedronBig, Debug.ColorSourcePerceived);
                 }
-            }
-
-            foreach (var (transformRO, positionRO, radiusRO) in SystemAPI
-                         .Query<RefRO<LocalToWorld>, RefRO<ComponentHearingPosition>, RefRO<ComponentHearingRadius>>()
-                         .WithAll<TagHearingSource, TagHearingDebug>()
-                         .WithAll<ComponentHearingDuration>())
-            {
-                var position = positionRO.ValueRO.Current;
-                var radius = math.sqrt(radiusRO.ValueRO.InternalCurrentSquared);
-
-                DebugAdvanced.DrawSphere(position, transformRO.ValueRO.Rotation, radius, debug.ColorSourceInternal);
-            }
-
-            foreach (var (transformRO, positionRO, radiusRO) in SystemAPI
-                         .Query<RefRO<LocalToWorld>, RefRO<ComponentHearingPosition>, RefRO<ComponentHearingRadius>>()
-                         .WithAll<TagHearingSource, TagHearingDebug>())
-            {
-                var position = positionRO.ValueRO.Current;
-                var radius = math.sqrt(radiusRO.ValueRO.CurrentSquared);
-
-                DebugAdvanced.DrawSphere(position, transformRO.ValueRO.Rotation, radius, debug.ColorSourceSphere);
-            }
-
-            foreach (var (positionRO, radiusRO) in SystemAPI
-                         .Query<RefRO<ComponentHearingPosition>, RefRO<ComponentHearingRadius>>()
-                         .WithAll<TagHearingSource, TagHearingDebug>()
-                         .WithAll<ComponentHearingDuration>()
-                         .WithNone<LocalToWorld>())
-            {
-                var position = positionRO.ValueRO.Current;
-                var radius = math.sqrt(radiusRO.ValueRO.InternalCurrentSquared);
-
-                DebugAdvanced.DrawSphere(position, quaternion.identity, radius, debug.ColorSourceInternal);
-            }
-
-            foreach (var (positionRO, radiusRO) in SystemAPI
-                         .Query<RefRO<ComponentHearingPosition>, RefRO<ComponentHearingRadius>>()
-                         .WithAll<TagHearingSource, TagHearingDebug>()
-                         .WithNone<LocalToWorld>())
-            {
-                var position = positionRO.ValueRO.Current;
-                var radius = math.sqrt(radiusRO.ValueRO.CurrentSquared);
-
-                DebugAdvanced.DrawSphere(position, quaternion.identity, radius, debug.ColorSourceSphere);
             }
         }
     }
