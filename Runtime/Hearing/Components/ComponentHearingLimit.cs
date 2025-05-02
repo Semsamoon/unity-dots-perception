e599c29a05ecef6ec18745438420358da7d357f4
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-02 22:12:35 +0300

    feat(hearing): add limit range calculation

--- Runtime/Hearing/Components/ComponentHearingLimit.cs
+++ Runtime/Hearing/Components/ComponentHearingLimit.cs
@@ -1,4 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
 
 namespace Perception
 {
@@ -6,11 +9,30 @@ namespace Perception
     /// Specifies limits for calculations per frame.
     /// Must be a singleton.
     /// </summary>
+    [BurstCompile]
     public struct ComponentHearingLimit : IComponentData
     {
         public int ChunksAmountPosition;
         public int ChunksAmountMemory;
         public int ChunksAmountSphere;
         public int ChunksAmountPerceive;
+
+        [BurstCompile]
+        public static void CalculateRanges(int limit, in NativeArray<int>.ReadOnly amounts, ref NativeArray<int2> ranges, ref int2 chunkIndexRange)
+        {
+            var sum = 0;
+            foreach (var amount in amounts)
+            {
+                sum += amount;
+            }
+
+            chunkIndexRange = chunkIndexRange.y >= sum ? new int2(0, limit) : new int2(chunkIndexRange.y, chunkIndexRange.y + limit);
+
+            for (var i = ranges.Length - 1; i >= 0; i--)
+            {
+                sum -= amounts[i];
+                ranges[i] = chunkIndexRange - sum;
+            }
+        }
     }
 }
