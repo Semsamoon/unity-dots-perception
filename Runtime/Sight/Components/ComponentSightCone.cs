a2d18f3f85a7777962586c7bb74317b405849da5
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-03 16:36:49 +0300

    refactor(sight): merge clip and filter with cone

--- Runtime/Sight/Components/ComponentSightCone.cs
+++ Runtime/Sight/Components/ComponentSightCone.cs
@@ -1,6 +1,7 @@
 ﻿using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 
 namespace Perception
@@ -10,20 +11,22 @@ namespace Perception
     /// </summary>
     public struct ComponentSightCone : IComponentData
     {
+        public CollisionFilter Filter;
         public float2 AnglesCos;
         public float RadiusSquared;
+        public float ClipSquared;
     }
 
     [BurstCompile]
     public static class ComponentSightConeExtensions
     {
         [BurstCompile]
-        public static bool IsInside(this in ComponentSightCone cone, in float3 origin, in float3 target, in LocalToWorld transform, float clipSquared = 0)
+        public static bool IsInside(this in ComponentSightCone cone, in float3 origin, in float3 target, in LocalToWorld transform)
         {
             var difference = target - origin;
             var distanceSquared = math.lengthsq(difference);
 
-            if (distanceSquared > cone.RadiusSquared || distanceSquared < clipSquared)
+            if (distanceSquared > cone.RadiusSquared || distanceSquared < cone.ClipSquared)
             {
                 return false;
             }
