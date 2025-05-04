3d1d2754bad165da40ad0f26f1aedbccf3369c43
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-04 10:19:27 +0300

    refactor: move collision filter serializable to common

---
+++ Runtime/Common/CollisionFilterSerializable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Unity.Mathematics;
+using Unity.Physics;
+using UnityEngine;
+
+namespace Perception
+{
+    [Serializable]
+    public struct CollisionFilterSerializable
+    {
+        public LayerMask BelongsTo;
+        public LayerMask CollidesWith;
+        public int GroupIndex;
+
+        public static implicit operator CollisionFilter(in CollisionFilterSerializable serializable)
+        {
+            return new CollisionFilter
+            {
+                BelongsTo = math.asuint(serializable.BelongsTo.value),
+                CollidesWith = math.asuint(serializable.CollidesWith.value),
+                GroupIndex = serializable.GroupIndex,
+            };
+        }
+
+        public static implicit operator CollisionFilterSerializable(in CollisionFilter filter)
+        {
+            return new CollisionFilterSerializable
+            {
+                BelongsTo = math.asint(filter.BelongsTo),
+                CollidesWith = math.asint(filter.CollidesWith),
+                GroupIndex = filter.GroupIndex,
+            };
+        }
+    }
+}
--- Runtime/Sight/Authorings/SightSenseAuthoring.cs
+++ Runtime/Sight/Authorings/SightSenseAuthoring.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Physics;
 using UnityEngine;
 
 namespace Perception
@@ -215,33 +213,5 @@ namespace Perception
             }
         }
 #endif
-    }
-
-    [Serializable]
-    public struct CollisionFilterSerializable
-    {
-        public LayerMask BelongsTo;
-        public LayerMask CollidesWith;
-        public int GroupIndex;
-
-        public static implicit operator CollisionFilter(in CollisionFilterSerializable serializable)
-        {
-            return new CollisionFilter
-            {
-                BelongsTo = math.asuint(serializable.BelongsTo.value),
-                CollidesWith = math.asuint(serializable.CollidesWith.value),
-                GroupIndex = serializable.GroupIndex,
-            };
-        }
-
-        public static implicit operator CollisionFilterSerializable(in CollisionFilter filter)
-        {
-            return new CollisionFilterSerializable
-            {
-                BelongsTo = math.asint(filter.BelongsTo),
-                CollidesWith = math.asint(filter.CollidesWith),
-                GroupIndex = filter.GroupIndex,
-            };
-        }
     }
 }
