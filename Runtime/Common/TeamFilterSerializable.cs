ac9797920f4816f14460f5055dbbacaa7fe3b4d9
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-04 09:56:03 +0300

    feat(common): add team filter serializable

---
+++ Runtime/Common/TeamFilterSerializable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Perception
+{
+    [Serializable]
+    public struct TeamFilterSerializable
+    {
+        public uint BelongsTo;
+        public uint Perceives;
+    }
+}
