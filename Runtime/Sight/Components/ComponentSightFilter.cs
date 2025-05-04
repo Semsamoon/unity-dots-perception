42a51d6da74369c0c749bbf9883d10bc9f134d96
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-04 09:38:21 +0300

    docs(sight): add team filter documentation

--- Runtime/Sight/Components/ComponentSightFilter.cs
+++ Runtime/Sight/Components/ComponentSightFilter.cs
@@ -2,6 +2,9 @@
 
 namespace Perception
 {
+    /// <summary>
+    /// Adds team filter to receivers and sources.
+    /// </summary>
     public struct ComponentSightFilter : IComponentData
     {
         public uint BelongsTo;
