d2d513ba93cd66440d03402494db50d6123f4c12
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-04 13:26:43 +0300

    feat(common): add team filter extensions

--- Runtime/Common/ComponentTeamFilter.cs
+++ Runtime/Common/ComponentTeamFilter.cs
@@ -1,13 +1,21 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Entities;
 
 namespace Perception
 {
     /// <summary>
     /// Adds team filter to receivers and sources.
     /// </summary>
+    [BurstCompile]
     public struct ComponentTeamFilter : IComponentData
     {
         public uint BelongsTo;
         public uint Perceives;
+
+        [BurstCompile]
+        public bool CanPerceive(in ComponentTeamFilter filter)
+        {
+            return (Perceives & filter.BelongsTo) > 0;
+        }
     }
 }
--- Runtime/Common/TeamFilterSerializable.cs
+++ Runtime/Common/TeamFilterSerializable.cs
@@ -8,6 +8,16 @@ namespace Perception
     {
         [TeamFilterMask] public uint BelongsTo;
         [TeamFilterMask] public uint Perceives;
+
+        public static implicit operator ComponentTeamFilter(in TeamFilterSerializable serializable)
+        {
+            return new ComponentTeamFilter { BelongsTo = serializable.BelongsTo, Perceives = serializable.Perceives };
+        }
+
+        public static implicit operator TeamFilterSerializable(in ComponentTeamFilter filter)
+        {
+            return new TeamFilterSerializable { BelongsTo = filter.BelongsTo, Perceives = filter.Perceives };
+        }
     }
 
     [AttributeUsage(AttributeTargets.Field)]
