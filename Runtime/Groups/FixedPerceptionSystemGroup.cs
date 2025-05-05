58f969d1b4c90a9f9746c22f97039c86d1c6ef4b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-05-05 19:53:35 +0300

    feat: add fixed step perception system group

---
+++ Runtime/Groups/FixedPerceptionSystemGroup.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Physics.Systems;
+
+namespace Perception
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup)), UpdateAfter(typeof(PhysicsSystemGroup))]
+    public partial class FixedPerceptionSystemGroup : ComponentSystemGroup
+    {
+    }
+}
